3f39b1f901c50a7cb6e63e6aecab4a714e81bdc4
MatveyChvikov <114420293+MatveyChvikov@users.noreply.github.com>
2025-02-28 08:35:45 +0300

    functions.h

--- functions.h
+++ functions.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <opencv2/opencv.hpp>
@@ -7,45 +6,64 @@
 #include <string>
 #include <nlohmann/json.hpp>
 
+#include <imgui.h>
+
 using json = nlohmann::json;
 
-// Структура для хранения результатов анализа изображения
+// Структура для результатов анализа
 struct ImageAnalysisResult {
     std::vector<double> edgeProfile;
     std::vector<double> noiseProfile;
-    double signalMean;
-    double noiseStd;
-    double cnr;
-    int centerX;
-    int centerY;
-    int radius;
+    double signalMean = 0.0;
+    double noiseStd = 0.0;
+    double cnr = 0.0;
+    int centerX = 0;
+    int centerY = 0;
+    int radius = 0;
 };
 
 // Глобальные переменные
 extern GLuint programID;
 extern GLuint vao;
-extern GLuint imageTexture;
-extern cv::Mat currentImage;
+
+extern cv::Mat images[2];
+extern cv::Mat* imagePtrs[2];
+
+extern cv::Mat*& currentImage;
+extern cv::Mat*& processedImage;
+
+extern GLuint textureIDs[2];
+extern GLuint* textureIDPtrs[2];
+
+extern GLuint*& currentImageID;
+extern GLuint*& processedImageID;
+
 extern std::vector<float> responseFunction;
 extern std::string outputMessage;
 extern ImageAnalysisResult currentAnalysis;
 
-// Функции обработки изображения
-void GenerateTestImage();
+extern ImVec2 resolution;
+
+// Функции
+void GenerateCustomCircle(int width, int height, int radius);
 void LoadImage();
 void CalculateResponseFunction();
-void ApplyEdgeEnhancement();
-void UpdateImageTexture();
-void RenderImage();
-void RenderResponseFunction();
+void UpdateImageTexture(const cv::Mat& from, GLuint& textureID);
+void RenderImage(const cv::Mat& from, const GLuint& textureID, const char* title);
 double CalculateNoiseLevel(const cv::Mat& image);
 double CalculateCNR(const cv::Mat& image, const cv::Rect& roi);
-void SynthesizeTestImage(int width, int height, int radius);
 ImageAnalysisResult AnalyzeImage(const cv::Mat& image, const cv::Point& center, int radius);
-json GetAnalysisData();
-
-// Функции визуализации
 void RenderAnalysisWindows();
 void RenderEdgeProfile();
 void RenderNoiseProfile();
 void RenderStatistics();
+json GetAnalysisData();
+
+void SwapImages();
+
+cv::Mat SharpenFilter(const cv::Mat& from);
+cv::Mat GaussBlurFilter(const cv::Mat& from);
+cv::Mat LaplaceOperator(const cv::Mat& from);
+cv::Mat NoiseFilter(const cv::Mat& from);
+
+void SaveImageToDisk(const cv::Mat& from, const char* path);
