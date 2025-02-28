ecc986ca91ea63370a0e44a4f599f2759e8b1eae
MatveyChvikov <114420293+MatveyChvikov@users.noreply.github.com>
2025-02-28 08:37:34 +0300

    Update FragmentShader.glsl

--- FragmentShader.glsl
+++ FragmentShader.glsl
@@ -6,7 +6,6 @@ in vec2 TexCoord;
 uniform sampler2D ourTexture;
 
 void main() {
-    // Преобразуем одноканальное изображение в оттенки серого
     float value = texture(ourTexture, TexCoord).r;
     FragColor = vec4(value, value, value, 1.0);
 }
